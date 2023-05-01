b170825a6b70930391aef740944da83f40b683a0
Jim Gerth <jim.frederic.gerth@gmail.com>
2023-05-01 11:19:02 +0200

    extensions/iterable_access: Add maybeLast getter
    Signed-off-by: Jim Gerth <jim.frederic.gerth@gmail.com>

--- lib/extensions/iterable_access.dart
+++ lib/extensions/iterable_access.dart
@@ -1,5 +1,7 @@
 import 'dart:math';
 
+import 'package:collection/collection.dart';
+
 /// An extension with shorthands for accessing different pars of [Iterable]s.
 extension IterableAccess<T> on Iterable<T> {
   /// Return the only element in `this` [Iterable] if it is available and alone.
@@ -7,6 +9,9 @@ extension IterableAccess<T> on Iterable<T> {
   /// If there is no element or there are more than one elements, this will
   /// return `null`.
   T? get maybeOnly => length == 1 ? first : null;
+
+  /// Return the last element of `this` [Iterable] (or `null` if it [isEmpty]).
+  T? get maybeLast => lastOrNull;
 
   /// Return a new [Iterable] without the last element of `this` [Iterable].
   Iterable<T> get withoutLast => take(max(length - 1, 0));
