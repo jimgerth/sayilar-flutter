64763dbf5ae53d9284acc5f79ce7873ab1612736
Jim Gerth <jim.frederic.gerth@gmail.com>
2023-05-01 11:13:25 +0200

    extensions/iterable_access: Add withoutLast getter
    Signed-off-by: Jim Gerth <jim.frederic.gerth@gmail.com>

--- lib/extensions/iterable_access.dart
+++ lib/extensions/iterable_access.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 /// An extension with shorthands for accessing different pars of [Iterable]s.
 extension IterableAccess<T> on Iterable<T> {
   /// Return the only element in `this` [Iterable] if it is available and alone.
@@ -5,4 +7,7 @@ extension IterableAccess<T> on Iterable<T> {
   /// If there is no element or there are more than one elements, this will
   /// return `null`.
   T? get maybeOnly => length == 1 ? first : null;
+
+  /// Return a new [Iterable] without the last element of `this` [Iterable].
+  Iterable<T> get withoutLast => take(max(length - 1, 0));
 }
