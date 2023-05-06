02767af3932f0cbe564b18890c022c7c288f6088
Jim Gerth <jim.frederic.gerth@gmail.com>
2023-05-06 10:24:16 +0200

    model/*_time_question: Add questions for telling time
    Signed-off-by: Jim Gerth <jim.frederic.gerth@gmail.com>

---
+++ lib/model/questions/translate_time_question.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import 'package:sayilar/extensions/written_out_time.dart';
+import 'package:sayilar/model/questions/question.dart';
+
+/// A [Question] in which a [time] should be translated by the user.
+///
+/// The numerical representation of a [time] will be shown to the user and they
+/// are expected to answer with the correct full length written out name of that
+/// that [time] *in turkish*.
+///
+/// For example when asked *"How do you write 13:00?"* the correct answer would
+/// be *"saat on üç"*.
+class TranslateTimeQuestion extends Question {
+  /// Create a new [TranslateTimeQuestion].
+  const TranslateTimeQuestion(this.time);
+
+  /// The time to be translated by the user.
+  final TimeOfDay time;
+
+  @override
+  String get question => 'How do you write *${time.asString}*?';
+
+  @override
+  String get answer => time.writtenOut;
+
+  @override
+  List<String> get alternateAnswers => time.alternateWrittenOut;
+}
