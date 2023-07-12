4f6a93a5d7546d139caf6a900a3cc687c05c93cf
Jim Gerth <jim.frederic.gerth@gmail.com>
2023-07-12 19:57:23 +0200

    model/brightness: Add HydratedBloc storing current brightness
    Signed-off-by: Jim Gerth <jim.frederic.gerth@gmail.com>

--- lib/model/brightness.dart
+++ lib/model/brightness.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:collection/collection.dart';
 import 'package:flutter_gen/gen_l10n/translations.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 
 /// The different possible brightness settings for the app.
 enum Brightness {
@@ -34,4 +36,31 @@ enum Brightness {
         dark => Translations.of(context).darkModeTooltip,
         light => Translations.of(context).lightModeTooltip,
       };
+}
+
+/// A [HydratedBloc] keeping track of and storing the current [Brightness].
+class BrightnessBloc extends HydratedBloc<Brightness, Brightness> {
+  /// Create a new [BrightnessBloc].
+  BrightnessBloc() : super(Brightness.first) {
+    on<Brightness>(
+      (brightness, emit) => emit(brightness),
+    );
+  }
+
+  /// Serialize the current [Brightness] into JSON format.
+  @override
+  Map<String, dynamic> toJson(Brightness state) => {
+        'brightness': state.name,
+      };
+
+  /// Parse a [Brightness] from the stored JSON data.
+  ///
+  /// If no [Brightness] could be parsed from the stored JSON data, this
+  /// defaults back to [Brightness.first].
+  @override
+  Brightness fromJson(Map<String, dynamic> json) =>
+      Brightness.values.singleWhereOrNull(
+        (value) => value.name == json['brightness'],
+      ) ??
+      Brightness.first;
 }
